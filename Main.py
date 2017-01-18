9b22816d57e67ae95df1a2a20a4a19de31a88516
raybalta <raymundbaltazar@yahoo.com>
2017-01-18 13:02:33 +1300

    added plotting class, can plot streams all or customised

--- Main.py
+++ Main.py
@@ -10,6 +10,7 @@ st = read('http://examples.obspy.org/RJOB_061005_072159.ehz.new')
 st1 = read('https://examples.obspy.org/COP.BHE.DK.2009.050')
 st2 = read('https://examples.obspy.org/COP.BHN.DK.2009.050')
 st3 = read('https://examples.obspy.org/COP.BHZ.DK.2009.050')
+size = (800,600)
 
 #print Test.Hello()
 #print Read_Seis.Read(st)
@@ -20,5 +21,6 @@ st3 = read('https://examples.obspy.org/COP.BHZ.DK.2009.050')
 #print Channel.Channel(st1)
 #print Channel.MultiChannel(st1, st2, st3)
 #Channel.Channel(st1).plot() ## plot data coming from a single channel
-Plotting.Pretty_Plot(st2,"blue", 10, 360)## send stream, number of ticks, color and time covered in seconds
-
+#Plotting.Pretty_Plot(st2,"blue", 10, 360)## send stream, number of ticks, color and time covered in seconds
+#Plotting.three_channel_Plot(st1, st2, st3, size)
+Plotting.Whole_Day_Plot(st1)
